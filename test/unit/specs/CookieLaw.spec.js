9ca93c7190ececccfd7d3ddd125a67d2ddedb3a8
Jakub Juszczak <netghost03@gmail.com>
2017-05-23 09:39:48 +0200

    🔥 Remove console log output from test

--- test/unit/specs/CookieLaw.spec.js
+++ test/unit/specs/CookieLaw.spec.js
@@ -5,7 +5,6 @@ describe('CookieLaw.vue', () => {
   it('should render correct contents', () => {
     const Constructor = Vue.extend(CookieLaw)
     const vm = new Constructor().$mount()
-    console.log(vm.$el.querySelector('.Cookie__content').textContent)
     expect(vm.$el.querySelector('.Cookie__content').textContent)
       .to.equal('This website uses cookies to ensure you get the best experience on our website.')
   })
