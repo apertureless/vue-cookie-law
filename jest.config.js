42e0063be3dfe5f100a0258ca7b22fbca0a41e95
Nans Pellicari <nans.pellicari@gmail.com>
2020-03-12 13:37:52 +0100

    Updates dependencies + use jest for test

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^~/(.*)$': '<rootDir>/src/$1',
+  },
+  moduleFileExtensions: ['js', 'vue', 'json'],
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+    '.*\\.(vue)$': 'vue-jest'
+  },
+  collectCoverage: true,
+  coverageDirectory: 'test/unit/coverage',
+  collectCoverageFrom: [
+    '<rootDir>/src/components/**/*.{js,vue}'
+  ],
+}
